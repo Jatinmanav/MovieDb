7d5cfbb5dd803cd6d2705a3aee0ecc775607874c
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-12-24 21:10:48 +0530

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,14 @@ import "../styles/header";
 const Header = () => {
   return (
     <div className="header-container">
-      <Logo className="logo" height={45} width={155} />
-      <div className="header-search-bar">
-        <SearchField />
-      </div>
-      <div className="header-profile">
-        <Person className="person-icon" height={45} width={45} />
+      <div className="header-content">
+        <Logo className="logo" height={30} width={135} />
+        <div className="header-search-bar">
+          <SearchField />
+        </div>
+        <div className="header-profile">
+          <Person className="person-icon" height={40} width={40} />
+        </div>
       </div>
     </div>
   );
